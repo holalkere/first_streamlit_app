2d38e0d5e616893bc426b7510cb544dc082bbb9d
P H <prakash.h@gmail.com>
2023-02-05 14:11:22 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
   streamlit.error()
   
 
-
+"""
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list_values('from streamlit')")
@@ -69,7 +69,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
   
-"""      
+      
 
 
 
